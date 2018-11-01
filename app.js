6457e51cc56d5c80c957e02ec447e14279b58fab
Elliott <elliottrecord@gmail.com>
2018-11-01 21:16:40 +0000

    Detecting environment.

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const app = express();
 
+// app.set('env', 'production');
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+}
 
 const Joi = require('joi');
 
