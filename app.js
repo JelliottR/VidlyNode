782e204f085688852d5bec0f1849533a2d2b9f0f
Elliott <elliottrecord@gmail.com>
2018-11-01 20:31:20 +0000

    Added DELETE /api/genres route.

--- app.js
+++ app.js
@@ -54,6 +54,23 @@ app.put('/api/genres', (req, res) => {
     });
 });
 
+app.delete('/api/genres', (req, res) => {
+    const schema = {
+        id: Joi.number().required(),
+    };
+
+    Joi.validate(req.body, schema, (err, value) => {
+        if (err) return res.status(400).send(err.details[0].message);
+
+        let genre = genres.find(genre => genre.id == req.body.id);
+        if (!genre) return res.status(404).send("The genre was not found.");
+
+        genres.pop(genre);
+        res.send(genres);
+    });
+
+});
+
 // PORT
 const port = process.env.PORT || 3000 
 app.listen(port, console.log(`Listening on port ${port}`));
