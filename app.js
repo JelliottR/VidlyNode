d616106bfc3b38ca12e55e696205b0abc5844bea
Elliott <elliottrecord@gmail.com>
2018-11-01 20:29:22 +0000

    Added PUT /api/genres route.

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ app.use(express.json())
 
 const Joi = require('joi');
 
-let genres = ['Action', 'Comedy'];
+let genres = [
+    {id: 1, name: 'Action'},
+    {id: 2, name: 'Comedy'}
+];
 
 app.get('/', (req, res) => {
     res.send('Home Page');
@@ -23,10 +26,32 @@ app.post('/api/genres', (req, res) => {
         if (err) return res.status(400).send(err.details[0].message);
 
         const name = req.body.name;
-        genres.push(name);
+        if(genres.find(g => g.name == name)){
+            return res.status(404).send("Genre already exists.")
+        }
+
+        const newGenre = {id: genres.length + 1, name};
+        genres.push(newGenre);
         res.send(value);
+                
     });
-        
+});
+
+app.put('/api/genres', (req, res) => {
+    const schema = {
+        id: Joi.number().required(),
+        name: Joi.string().min(3).required()
+    }
+
+    Joi.validate(req.body, schema, (err, value) => {
+        if (err) return res.status(400).send(err.details[0].message);
+
+        let genre = genres.find(genre => genre.id == req.body.id);
+        if(!genre) return res.status(404).send("The genre was not found.");
+
+        genre.name = req.body.name;
+        res.send(genres);
+    });
 });
 
 // PORT
