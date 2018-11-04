cfd9965b5f0433751efd174fb547ccd474d57d9d
Elliott <elliottrecord@gmail.com>
2018-11-04 21:38:55 +0000

    Generate a JWT token on sign in.

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { User } = require('../models/User')
 const _ = require('lodash');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
@@ -15,7 +16,9 @@ router.post('/', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    res.send(true);
+    const token = jwt.sign({_id: user._id}, 'jwtPrivateKey');
+
+    res.send(token);
     
 });
 
