143529dc68cc209eff7161528683d103d1d7dd28
Elliott <elliottrecord@gmail.com>
2018-11-05 20:39:42 +0000

    Cleaned up routes from app.js.

--- app.js
+++ app.js
@@ -3,25 +3,16 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const debug = require('debug')('app:startup');
 const config = require('config');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-// process.on('uncaughtException', (ex)=>{
-//     winston.error(ex.message, ex);
-//     process.exit(1);
-// });
+require('./startup/routes')(app);
 
 winston.add(new winston.transports.File({
     filename: 'uncaughtExceptions.log',
@@ -32,27 +23,10 @@ process.on('unhandledRejection', (ex) => {
     throw ex;
 })
 
-
 winston.add(new winston.transports.File( {filename: 'logfile.log'} ));
 winston.add(new winston.transports.MongoDB({db: 'mongodb://localhost/Vidly', level:'error'}));
 
 app.set('view engine', 'pug');
-throw new Error('Test');
-const p = Promise.reject(new Error('failed.'));
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use(helmet());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
-
 
 if(app.get('env') === 'development'){
     app.use(morgan('tiny'));
@@ -67,10 +41,6 @@ if (!config.get('jwtPrivateKey')){
 mongoose.connect('mongodb://localhost/Vidly', { useNewUrlParser: true })
     .then(console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
-
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Title of Tab', message: 'Hello World!' });
-});
 
 // PORT
 const port = process.env.PORT || 3000 
